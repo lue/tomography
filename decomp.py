dba0f85a87574b60a3bc791e4f1b91dc41b37fde
Alexander Kaurov <akaurov@gmail.com>
2016-06-07 15:55:06 -0500

    upd

--- decomp.py
+++ decomp.py
@@ -3,15 +3,18 @@ import matplotlib.pyplot as plt
 from tools.io import *
 from tools.smooth import *
 
-a_list = ['a=0.']
+a_list =     ['0.0905','0.1000','0.1118','0.1208','0.1281']
+xi_list =    ['99.5'  , '97.5' , '73.3' ,'26.9',  '3.16']
+a_list_cal = ['0.0905','0.1000','0.1118','0.1118','0.1281']
+a_i = 4
 
-N1,N2,N3,t = readifrit('E:\REI\Cai.B80.N512L2.sf=1_uv=0.15_bw=10_res=200.NMA\B\IFRIT\ifrit-a=0.1000.bin', nvar=4, moden=2, skipmoden=2)
+N1,N2,N3,t = readifrit('E:\REI\Cai.B80.N512L2.sf=1_uv=0.15_bw=10_res=200.NMA\B\IFRIT\ifrit-a=' + a_list[a_i] + '.bin', nvar=4, moden=2, skipmoden=2)
 
 # plt.imshow(np.log10(t[:,:,0]), interpolation='nearest')
+# plt.colorbar()
 
-
-halos = np.genfromtxt('E:\REI\Cai.B80.N512L2.sf=1_uv=0.15_bw=10_res=200.NMA\B\\a=0.1000\halo_catalog_a0.1000.dat', comments='#', dtype=None)
-Lcat = np.genfromtxt('E:\REI\Cai.B80.N512L2.sf=1_uv=0.15_bw=10_res=200.NMA\B\\a=0.1000\gallums.res', comments='#', dtype=None)
+halos = np.genfromtxt('E:\REI\Cai.B80.N512L2.sf=1_uv=0.15_bw=10_res=200.NMA\B\\a=' + a_list_cal[a_i] + '\halo_catalog_a' + a_list_cal[a_i] + '.dat', comments='#', dtype=None)
+Lcat = np.genfromtxt('E:\REI\Cai.B80.N512L2.sf=1_uv=0.15_bw=10_res=200.NMA\B\\a=' + a_list_cal[a_i] + '\gallums.res', comments='#', dtype=None)
 
 x = halos['f3']/80.*1024.
 y = halos['f2']/80.*1024.
@@ -24,8 +27,8 @@ for i in range(len(Lcat)):
     print(ind)
 
 
-plt.imshow(np.log10(t[:,:,0].T), origin='lower', interpolation='nearest', cmap='jet')
-plt.plot(pos[(pos[:,2]<15),0], pos[(pos[:,2]<15),1], '.k')
+# plt.imshow(np.log10(t[:,:,0].T), origin='lower', interpolation='nearest', cmap='jet')
+# plt.plot(pos[(pos[:,2]<15),0], pos[(pos[:,2]<15),1], '.k')
 
 
 #########################
@@ -46,6 +49,7 @@ pos_r = np.floor(pos).astype(int)
 gal21[:,0] = t[pos_r[:,0],pos_r[:,1],pos_r[:,2]]
 
 t = downsample(t)
+t /= 8
 
 pos /= 2.0
 pos_r = np.floor(pos).astype(int)
@@ -60,35 +64,40 @@ for i in range(len(sm_res)):
 
 R_list = 2**np.arange(10)*80/1024
 
-II = 6
-LNbins = 9
-Lbins = np.percentile(magAB, np.linspace(0, 1, LNbins)*100)
-BNbins = 9
-Bbins = np.percentile(gal21[:,II], np.linspace(0, 1, BNbins)*100)
-H = np.histogram2d(magAB, gal21[:,II], bins=[Lbins, Bbins])
-img = H[0] / len(Lcat) * (LNbins-1) * (BNbins-1)
-img = (img - 1.0) * 100.
-
-fig = plt.figure()
-rect = 0.2, 0.1, 0.8, 0.8
-ax1 = fig.add_axes(rect)
-temp = ax1.imshow(img, extent=[0,100,0,100], interpolation='nearest', cmap='bwr', vmin=-20, vmax=20, origin='lower', aspect='auto')
-# ax1.yaxis.tick_right()
-# ax1.yaxis.set_label_position('right')
-# ax1.xaxis.tick_top()
-# ax1.xaxis.set_label_position('top')
-plt.ylabel('Luminosity (percentiles)')
-plt.xlabel('21cm brightness (percentiles)')
-plt.colorbar(temp)
-plt.title('z=10 \n Smoothing scale: '+str(R_list[II])+' Mpc/h')
-
-# ax2 = fig.add_axes(rect, frameon=False)
-# plt.xlim([0,1])
-Bbins_labels = ["%2.1f\n%d"%(Bbins[i]/8/8*1000, 100.*i/(len(Bbins)-1))+'%' for i in range(len(Bbins))]
-plt.xticks(np.linspace(0,100,BNbins), Bbins_labels, rotation='horizontal')
-# plt.ylim([0,1])
-Lbins_labels = ["%2.1f\n%d"%(Lbins[i], 100.*i/(len(Lbins)-1))+'%' for i in range(len(Lbins))]
-plt.yticks(np.linspace(0,100,LNbins), Lbins_labels, rotation='horizontal')
-plt.ylabel('Luminosity [erg/s/Hz]')
-plt.xlabel('21cm brightness [mK]')
+for II in [0,1,2,3,4,5,6,7,8]:
+    LNbins = 17
+    Lbins = np.percentile(magAB, np.linspace(0, 1, LNbins)*100)
+    BNbins = 11
+    Bbins = np.percentile(gal21[:,II], np.linspace(0, 1, BNbins)*100)
+    H = np.histogram2d(magAB, gal21[:,II], bins=[Lbins, Bbins])
+    img = H[0] / len(Lcat) * (LNbins-1) * (BNbins-1)
+    img = (img - 1.0) * 100.
+    fig = plt.figure()
+    rect = 0.2, 0.15, 0.8, 0.75
+    ax1 = fig.add_axes(rect)
+    temp = ax1.imshow(img,
+                      extent=[0,100,0,100],
+                      interpolation='nearest',
+                      cmap='bwr',
+                      vmin=-np.ceil(np.max(np.abs(img))), vmax=np.ceil(np.max(np.abs(img))),
+                      origin='lower', aspect='auto')
+    # ax1.yaxis.tick_right()
+    # ax1.yaxis.set_label_position('right')
+    # ax1.xaxis.tick_top()
+    # ax1.xaxis.set_label_position('top')
+    plt.ylabel('Luminosity (percentiles)')
+    plt.xlabel('21cm brightness (percentiles)')
+    plt.colorbar(temp)
+    plt.title(('z=%2.1f xHI='+xi_list[a_i]+'\nSmoothing scale: %1.2f Mpc/h')%(1.0/float(a_list[a_i])-1., R_list[II]))
+    # ax2 = fig.add_axes(rect, frameon=False)
+    # plt.xlim([0,1])
+    Bbins_labels = ["%2.1f\n%d"%(Bbins[i]*1000, 100.*i/(len(Bbins)-1))+'%' for i in range(len(Bbins))]
+    plt.xticks(np.linspace(0,100,BNbins), Bbins_labels, rotation='horizontal', size=8)
+    # plt.ylim([0,1])
+    Lbins_labels = ["%2.1f\n%d"%(Lbins[i], 100.*i/(len(Lbins)-1))+'%' for i in range(len(Lbins))]
+    plt.yticks(np.linspace(0,100,LNbins), Lbins_labels, rotation='horizontal', size=8)
+    plt.ylabel('Magnitude AB')
+    plt.xlabel('21cm brightness [mK]')
+    plt.savefig(('images/z%02.1f_%02.2fmpch_2'%(1.0/float(a_list[a_i])-1., R_list[II])).replace('.','_')+'.png', dpi=200,bbox_inches='tight')
+    plt.close('all')
 
